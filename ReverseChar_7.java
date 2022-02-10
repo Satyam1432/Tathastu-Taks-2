3b14ace74afbe07225682ceeb3178e466cd1b184
Satyam1432 <satyamthorat01@gmail.com>
2022-02-10 16:39:52 -0500

    first commit

---
+++ ReverseChar_7.java
@@ -0,0 +1,26 @@
+public class ReverseChar_7 {
+    // java program to reverse a word
+
+
+
+
+	public static void main (String[] args) {
+	
+		String str= "Geeks", nstr="";
+		char ch;
+	
+	System.out.print("Original word: ");
+	System.out.println("Geeks"); //Example word
+	
+	for (int i=0; i<str.length(); i++)
+	{
+		ch= str.charAt(i); //extracts each character
+		nstr= ch+nstr; //adds each character in front of the existing string
+	}
+	System.out.println("Reversed word: "+ nstr);
+	}
+}
+
+//Contributed by Tiyasa
+
+
